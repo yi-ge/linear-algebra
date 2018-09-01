6c4c52479ca4c1d7fe33299973b26e8a7aad96d9
轶哥 <a@wyr.me>
2018-09-01 16:13:13 +0800

    Vector add

--- main_vector.py
+++ main_vector.py
@@ -1,6 +1,20 @@
+import sys
+import numpy
+import scipy
+
 from playLA.Vector import Vector
 
 if __name__ == "__main__":
 
+    print(sys.version)
+    print(numpy.__version__)
+    print(scipy.__version__)
+
     vec = Vector([1, 3])
     print(vec)
+    print("len(vec) = {}".format(len(vec)))
+    print("vec[0] = {}, vec(1) = {}".format(vec[0], vec[1]))
+
+    vec1 = Vector([2, 3])
+    print("vec + vec1 = {}".format(vec + vec1))
+    print("vec - vec1 = {}".format(vec + vec1))
--- playLA/Vector.py
+++ playLA/Vector.py
@@ -2,6 +2,9 @@ class Vector:
     def __init__(self, lst):
         self._values = lst
 
+    def __add__(self, other):
+        return Vector([a + b for a, b in zip(self, other)])
+
     def __getitem__(self, index):
         """取向量第一个元素"""
         return self._values[index]
