5736c3b6666c308213ecaa5b1a92d01866932a40
轶哥 <a@wyr.me>
2018-09-01 15:12:55 +0800

    Vector

---
+++ main_vector.py
@@ -0,0 +1,6 @@
+from playLA.Vector import Vector
+
+if __name__ == "__main__":
+
+    vec = Vector([1, 3])
+    print(vec)
---
+++ playLA/Vector.py
@@ -0,0 +1,19 @@
+class Vector:
+    def __init__(self, lst):
+        self._values = lst
+
+    def __getitem__(self, index):
+        """取向量第一个元素"""
+        return self._values[index]
+
+    def __len__(self):
+        """返回向量长度"""
+        return len(self._values)
+
+    def __repr__(self):
+        """迭代器"""
+        return "Vector({})".format(self._values)
+
+    def __str__(self):
+        return "({})".format(", ".join(str(e) for e in self._values))
+
